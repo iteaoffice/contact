aa9a2dee2e1c975dd87c34edb11a7bd82eb1839f
japaveh <info@japaveh.nl>
2013-08-22 14:05:20 +0200

    Fixed the correction on the navigation

--- config/module.config.navigation.php
+++ config/module.config.navigation.php
@@ -9,63 +9,27 @@
  */
 return array(
     'navigation' => array(
-        'default' => array(
-            'contact' => array(
-                'label' => _("txt-contact"),
-                'route' => 'contact',
-                'pages' => array(
-                    'contacts'      => array(
-                        'label'     => _("txt-list-contacts"),
-                        'route'     => 'contact/contacts',
-                        'resource'  => 'contact',
-                        'privilege' => 'listings',
-                    ),
-                    'facilities'     => array(
-                        'label'     => _("txt-list-facilities"),
-                        'route'     => 'contact/facilities',
-                        'resource'  => 'contact',
-                        'privilege' => 'listings',
-                    ),
-                    'areas'          => array(
-                        'label'     => _("txt-list-areas"),
-                        'route'     => 'contact/areas',
-                        'resource'  => 'contact',
-                        'privilege' => 'listings',
-                    ),
-                    'sub-areas'      => array(
-                        'label'     => _("txt-list-sub-areas"),
-                        'route'     => 'contact/sub-areas',
-                        'resource'  => 'contact',
-                        'privilege' => 'listings',
-                    ),
-                    'area2s'         => array(
-                        'label'     => _("txt-list-area2s"),
-                        'route'     => 'contact/area2s',
-                        'resource'  => 'contact',
-                        'privilege' => 'listings',
-                    ),
-                    'oper-areas'     => array(
-                        'label'     => _("txt-list-operation-areas"),
-                        'route'     => 'contact/oper-areas',
-                        'resource'  => 'contact',
-                        'privilege' => 'listings',
-                    ),
-                    'oper-sub-areas' => array(
-                        'label'     => _("txt-list-operation-sub-areas"),
-                        'route'     => 'contact/oper-sub-areas',
-                        'resource'  => 'contact',
-                        'privilege' => 'listings',
-                    ),
-                ),
-            ),
-            'admin'    => array(
-                'pages' => array(
-                    'messages' => array(
-                        'label' => _('txt-messages'),
-                        'route' => 'zfcadmin/contact-manager/messages',
-                    ),
-                ),
-            ),
-        ),
+//        'default' => array(
+//            'contact' => array(
+//                'label' => _("txt-contact"),
+//                'route' => 'contact',
+//                'pages' => array(
+//                    'contacts' => array(
+//                        'label'     => _("txt-list-contacts"),
+//                        'route'     => 'contact/contacts',
+//                        'resource'  => 'contact',
+//                        'privilege' => 'listings',
+//                    ),
+//                ),
+//            ),
+//            'admin'   => array(
+//                'pages' => array(
+//                    'messages' => array(
+//                        'label' => _('txt-messages'),
+//                        'route' => 'zfcadmin/contact-manager/messages',
+//                    ),
+//                ),
+//            ),
+//        ),
     ),
 );
