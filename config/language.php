4ff1001867e8378a8340698c2e397cb063089723
japaveh <info@japaveh.nl>
2014-01-03 22:53:21 +0100

    Update on the contacts

---
+++ config/language.php
@@ -0,0 +1,17 @@
+<?php
+_("txt-not-hide-for-others");
+_("txt-hide-for-others");
+_("txt-not-hide-photo");
+_("txt-hide-photo");
+_("txt-visibility-hidden");
+_("txt-visibility-community");
+_("txt-visibility-public");
+_("txt-attention");
+_("txt-first-name");
+_("txt-middle-name");
+_("txt-last-name");
+_("txt-phone-number");
+_("txt-zip-code");
+_("txt-city");
+_("txt-visibility");
+_("txt-photo-file");
