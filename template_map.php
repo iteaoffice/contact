0bce04f6384fb17d587a995a74eb3cd8af9d50a7
japaveh <info@japaveh.nl>
2014-01-10 11:31:10 +0100

    Update on the signature

--- template_map.php
+++ template_map.php
@@ -4,6 +4,7 @@ return array(
     'contact/contact/change-password'     => __DIR__ . '//view/contact/contact/change-password.twig',
     'contact/contact/profile-edit'        => __DIR__ . '//view/contact/contact/profile-edit.twig',
     'contact/contact/profile'             => __DIR__ . '//view/contact/contact/profile.twig',
+    'contact/contact/signature'           => __DIR__ . '//view/contact/contact/signature.twig',
     'contact/contact-manager/edit'        => __DIR__ . '//view/contact/contact-manager/edit.twig',
     'contact/contact-manager/impersonate' => __DIR__ . '//view/contact/contact-manager/impersonate.twig',
     'contact/contact-manager/list'        => __DIR__ . '//view/contact/contact-manager/list.twig',
@@ -12,4 +13,5 @@ return array(
     'contact/partial/entity/contact'      => __DIR__ . '//view/contact/partial/entity/contact.twig',
     'contact/partial/form/contact'        => __DIR__ . '//view/contact/partial/form/contact.twig',
     'contact/partial/pagination-control'  => __DIR__ . '//view/contact/partial/pagination-control.twig',
+    'contact/partial/signature'           => __DIR__ . '//view/contact/partial/signature.twig',
 );
