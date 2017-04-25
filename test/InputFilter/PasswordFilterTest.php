0d9c4a1b318004337c2a89a194de46c07b17d54d
Johan van der Heide <info@japaveh.nl>
2017-04-25 10:15:31 +0200

    Pre-release code commit

---
+++ test/InputFilter/PasswordFilterTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * ITEA Office all rights reserved
+ *
+ * @category    Contact
+ *
+ * @author      Johan van der Heide <johan.van.der.heide@itea3.org>
+ * @copyright   Copyright (c) 2004-2017 ITEA Office (https://itea3.org)
+ */
+
+namespace ContactTest\Service;
+
+use Contact\InputFilter\PasswordFilter;
+use Testing\Util\AbstractInputFilterTest;
+
+/**
+ * Class PasswordFilterTest
+ * @package ContactTest\Service
+ */
+class PasswordFilterTest extends AbstractInputFilterTest
+{
+    /**
+     * Set up basic properties
+     */
+    public function setUp()
+    {
+    }
+
+    /**
+     *
+     */
+    public function testCanCreatePasswordFilterInputFilter()
+    {
+        $passwordFilter = new PasswordFilter($this->getEntityManagerMock());
+
+        $this->assertInstanceOf(PasswordFilter::class, $passwordFilter);
+    }
+
+
+    /**
+     *
+     */
+    public function testContactInputFilterHasElements()
+    {
+        $passwordFilter = new PasswordFilter($this->getEntityManagerMock());
+
+        $this->assertInstanceOf(PasswordFilter::class, $passwordFilter);
+
+        $this->assertNotNull($passwordFilter->get('password'));
+        $this->assertNotNull($passwordFilter->get('passwordVerify'));
+    }
+}
---
+++ test/Service/FormServiceTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * ITEA Office all rights reserved
+ *
+ * PHP Version 7
+ *
+ * @category    Project
+ *
+ * @author      Johan van der Heide <johan.van.der.heide@itea3.org>
+ * @copyright   Copyright (c) 2004-2017 ITEA Office (https://itea3.org)
+ * @license     https://itea3.org/license.txt proprietary
+ *
+ * @link        http://github.com/iteaoffice/project for the canonical source repository
+ */
+
+namespace ContactTest\Service;
+
+use Contact\Entity\Contact;
+use Contact\Service\FormService;
+use Testing\Util\AbstractServiceTest;
+use Zend\Form\Form;
+
+/**
+ * Class ContactServiceTest
+ *
+ * @package ContactTest\Service
+ */
+class FormServiceTest extends AbstractServiceTest
+{
+    /**
+     *
+     */
+    public function testCanCreateService()
+    {
+        $service = new FormService();
+        $this->assertInstanceOf(FormService::class, $service);
+    }
+
+    public function testCanPrepare()
+    {
+        $this->markTestIncomplete("To be continued");
+
+        $service = new FormService();
+
+        $serviceManager = $this->getServiceManager();
+        $service->setEntityManager($this->getEntityManagerMock());
+
+
+        $service->setServiceLocator($serviceManager);
+
+        $this->assertInstanceOf(Form::class, $service->prepare(Contact::class));
+    }
+
+}
